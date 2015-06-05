efd1d8a34108c319d04d7049562b50cc1c644e10
sijin cao <scao@acquisio.com>
2015-06-05 14:43:24 -0400

    basic app structure

---
+++ src/app.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var SampleComponent = require('./components/sample');
+
+React.render(
+    <SampleComponent/>,
+    document.body
+)
--- src/components/sample.js
+++ src/components/sample.js
@@ -1,12 +1,10 @@
 /** @jsx React.DOM */
 var React = require('react');
 var requireStylesheet = require('stylesheets').requireStylesheet;
-
 requireStylesheet(process.env.STATIC_ROOT + 'MyComponent.css');
-
 var SampleComponent = React.createClass({
   render: function() {
-    return <span className="MyComponent">Hello, This is a sample component!</span>;
+    return <span className="MyComponent">Hello, Continuous build!</span>;
   }
 });
 
